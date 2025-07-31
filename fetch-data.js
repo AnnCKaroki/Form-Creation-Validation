df1b567ae9adbfd291355063113189ceb774e416
Ann <annkarokiadmissions@gmail.com>
2025-07-31 18:17:50 +0300

    Add asynchronous javascript to fetch user data from an api and display user name

--- fetch-data.js
+++ fetch-data.js
@@ -1,7 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
-    async function fetchUseData() {
+    async function fetchUserData() {
         const apiUrl = 'https://jsonplaceholder.typicode.com/users';
-        
 
+    const dataContainer = document.getElementById('api-data');
+
+    try {
+        const response = await fetch(apiUrl)
+        const users = await response.json()
+        dataContainer.innerHTML = ''
+
+        const userList = document.createElement('ul')
+        users.forEach(user => {
+            const listItem = document.createElement('li')
+            listItem.textContent = users.name
+            userList.appendChild(listItem)
+
+        });
+
+    } catch (error) {
+        if (dataContainer) {
+            dataContainer.innerHTML=''
+            dataContainer.textContent='Failed to load user data.'
+        }
+    }
     };
+
+fetchUserData();
+
 })
