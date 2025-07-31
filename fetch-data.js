4bb086a41814807f4e57ec361292b657b2e9a39b
Ann <annkarokiadmissions@gmail.com>
2025-07-31 15:46:09 +0300

    Initialize asynchronous function

---
+++ fetch-data.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    async function fetchUseData() {
+        const apiUrl = 'https://jsonplaceholder.typicode.com/users';
+        
+
+    };
+})
